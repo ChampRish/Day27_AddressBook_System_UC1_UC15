f7a023b61b0bf186ad3c8f3c725ea3573351e534
RISHABH <gguptarishabh82@gmail.com>
2022-04-05 01:42:27 +0530

    Ability to create a Contact in Address Book

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day27_AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("!------Welcome to Address Book system------!");
+            //creating object of addressbook
+            UC1_CreateAContactInAddressBook address =new UC1_CreateAContactInAddressBook();
+
+            //getting Op in Constructor with paratmers
+            address.addContacts("Rishabh", "Gupta", "Agra CAntt", "Agra", "UP", 12345, 01234567890, "abc@gmail.com");
+            // called Print method 
+            address.print();
+            Console.ReadLine();
+
+        }
+    }
+}
+    
+
---
+++ UC1-CreateAContactInAddressBook.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day27_AddressBookSystem
+{
+     class UC1_CreateAContactInAddressBook
+     {
+        private List<Contact> contactList;
+
+        public void AddressBook()
+        {
+            contactList = new List<Contact>();
+        }
+
+        public void addContacts(string firstName, string lastName, string address, string city, string state, int zip, long phoneNumber, string email)
+        {
+            //Create object of Contact class
+            Contact contact = new Contact();
+            //calling Varivale using object
+            contact.firstname = firstName;
+            contact.lastname = lastName;
+            contact.address = address;
+            contact.city = city;
+            contact.state = state;
+            contact.zip = zip;
+            contact.phonenumber = phoneNumber;
+            contact.emailid = email;
+            //adding contact details in contact list 
+            contactList.Add(contact);
+        }
+        //method print
+        public void print()
+        {
+            //using foreach loop for calling the variable 
+            foreach (Contact contact in contactList)
+            {
+                //Prinitng the Op
+                Console.WriteLine("FirstName: " + contact.firstname);
+                Console.WriteLine("LastName: " + contact.lastname);
+                Console.WriteLine("Address: " + contact.address);
+                Console.WriteLine("City: " + contact.city);
+                Console.WriteLine("State: " + contact.state);
+                Console.WriteLine("Zip: " + contact.zip);
+                Console.WriteLine("PhoneNumber: " + contact.phonenumber);
+                Console.WriteLine("Email id: " + contact.emailid);
+            }
+        }
+    }
+}
